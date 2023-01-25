ef2a266c840eb40b493d78a4f0646babb3724cbc
Jesus Vasquez <jesusvasquezesc7@gmail.com>
2023-01-25 13:02:36 -0400

    creating a github repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bulma/css/bulma.css';
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
+function App(){
+    return(
+        <div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal digital asistant</p>
+                </div>
+            </section>
+            <div>
+                <div className='container'>
+                    <section className='section'>
+                        <div className='columns'>
+                            <div className='column is-4'>
+                                <ProfileCard 
+                                title="Alexa" 
+                                handle="@alexa99" 
+                                image={AlexaImage}
+                                description="Alexa is a Amazon app and helps you buy things"/>
+                            </div>
+                            <div className='column is-4'>
+                                <ProfileCard 
+                                title="Cortana" 
+                                handle="@cortana45" 
+                                image={CortanaImage}
+                                description="Cortana is a microsoft app and helps to search"/>
+                            </div>
+                            <div className='column is-4'>
+                                <ProfileCard 
+                                title="Siri" 
+                                handle="@siri8" 
+                                image={SiriImage}
+                                description="Siri is a apple app and is out of bussiness"/>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                
+                
+               
+            </div>
+        </div>
+    );
 }
-
 export default App;
